7fd2c2c9ac2f5ab2904da6cfd62183f9cdb87530
Jasmine <jasmineallen1107@gmail.com>
2021-08-18 06:40:39 -0500

    Primitive functions created

---
+++ calculator.js
@@ -0,0 +1,23 @@
+//Make functions for each: add, subtract, multiply, divide.
+//They each take two values.
+
+
+let number;
+let number2;
+
+let add = (n1, n2) => {
+    return n1 + n2;
+};
+
+let subtract = (n1, n2) => {
+    return n1 - n2;
+};
+
+let multiply = (n1, n2) => {
+    return n1 * n2;
+};
+
+let divide = (n1, n2) => {
+    return n1 / n2;
+};
+
