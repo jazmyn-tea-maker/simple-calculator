8a01b0dd6f98a5c58598abadfda66f84bdd9f083
Jasmine <jasmineallen1107@gmail.com>
2021-08-18 06:46:12 -0500

    Operate function created

--- calculator.js
+++ calculator.js
@@ -5,19 +5,31 @@
 let number;
 let number2;
 
-let add = (n1, n2) => {
-    return n1 + n2;
-};
+//Connect each button to each operation string.
 
-let subtract = (n1, n2) => {
-    return n1 - n2;
-};
+let operate = (operation, num1, num2) => {
 
-let multiply = (n1, n2) => {
-    return n1 * n2;
-};
+    let add = (n1, n2) => {
+        return n1 + n2;
+    };
 
-let divide = (n1, n2) => {
-    return n1 / n2;
-};
+    let subtract = (n1, n2) => {
+        return n1 - n2;
+    };
+
+    let multiply = (n1, n2) => {
+        return n1 * n2;
+    };
+
+    let divide = (n1, n2) => {
+        return n1 / n2;
+    };
+
+    return operation == 'add' ? add(num1, num2) :
+    operation == 'subtract' ? subtract(num1, num2) :
+    operation == 'multiply' ? multiply(num1, num2) :
+    operation == 'divide' ? divide(num1, num2) :
+    false;
+}
+
 
